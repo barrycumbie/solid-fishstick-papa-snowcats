c514c3e4f5f4a61552acf5ad73b082b680cd852b
Barry <barrycumbie@gmail.com>
2024-02-20 09:25:07 -0600

    getting spun

--- app.js
+++ app.js
@@ -1,14 +1,66 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5500;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const bodyParser = require('body-parser')
+
 // set the view engine to ejs
 let path = require('path');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // use res.render to load up an ejs view file
 
 let myTypeServer = "9️⃣ The Peacemaker ✌🏻";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    const result = await client.db("papa-database").collection("papa-collection").find().toArray();
+
+    //console.log("cxnDB result: ", result);
+
+    return result; 
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+//run().catch(console.dir);
+
+app.get('/read', async (req,res) => {
+
+  let myResultServer = await run(); 
+
+  console.log("myResultServer:", myResultServer[0].userName);
+
+
+
+  res.render('index', {
+    myTypeClient: myTypeServer,
+    myResultClient: myResultServer
+
+  });
+
+
+}); 
+
 
 app.get('/', function(req, res) {
 
@@ -26,8 +78,10 @@ app.get('/send', function (req, res) {
     res.send('Hello World from Express <br><a href="/">home</a>')
 })
 
+
+
 // app.listen(3000)
 
 app.listen(port, () => {
-  console.log(`nov app listening on port ${port}`)
+  console.log(`papa app listening on port ${port}`)
 })
